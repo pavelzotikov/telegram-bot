6a5eabb4cd37ddb0ed0431ada3ea628e22b1b6f0
Pavel Zotikov <pavelzotikov@gmail.com>
2019-09-30 23:24:50 +0300

    Change «Memcache» -> «Memcached»

--- src/TelegramBot/Handler.php
+++ src/TelegramBot/Handler.php
@@ -4,7 +4,7 @@ namespace TelegramBot;
 
 class Handler
 {
-    /** @var \Memcache */
+    /** @var \Memcached */
     protected $cache;
 
     /** @var array */
@@ -78,10 +78,10 @@ class Handler
         return 'Catcher остановлен';
     }
 
-    public function getCacheInstance(): \Memcache
+    public function getCacheInstance(): \Memcached
     {
         if (!$this->cache) {
-            $this->cache = new \Memcache();
+            $this->cache = new \Memcached();
             $this->cache->addServer('localhost', 11211);
         }
 
